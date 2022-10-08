9c2299a45c1b9c390353483dd2f75d75321876d6
Nathaniel Daniel <nathaniel.daniel12@gmail.com>
2022-10-07 23:02:12 -0700

    fmt

--- msys2-gtk-packager/src/main.rs
+++ msys2-gtk-packager/src/main.rs
@@ -99,8 +99,7 @@ impl Context {
                     .join("bin")
                     .join("pkg-config")
                     .into(),
-            ) 
-            /*
+            )
             // TODO: If the user specifies RUSTFLAGS, extend it. If they specify a linker, noop here. Make this work for cross-comp.
             .env(
                 "RUSTFLAGS".into(),
@@ -109,11 +108,10 @@ impl Context {
                     msys2_installation_path
                         .join(&env_sysroot)
                         .join("bin")
-                        .join("ld")
+                        .join("gcc")
                 )
                 .into(),
             )
-            */
             .env(
                 // TODO: Consider ripping out pkg-config and locating all these libs manually so users can use pkg-config for other stuff, or extend those env vars.
                 "PKG_CONFIG_SYSROOT_DIR".into(),
